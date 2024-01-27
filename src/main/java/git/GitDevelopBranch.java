40577d950610c84076f6b46d431e34c3e037ae8e
SagarQATester <sagar95kumbhar@gmail.com>
2024-01-27 23:58:31 +0530

    after developer brach code merged with master

--- src/main/java/git/GitDevelopBranch.java
+++ src/main/java/git/GitDevelopBranch.java
@@ -9,6 +9,7 @@ public class GitDevelopBranch {
 		System.out.println("Code created in develop branch");
 		System.out.println("Code created by USA team for develop branch ");
 		System.out.println("develop code merged to master branch and pushed ");
+		System.out.println("pushed code to master branch");
 		
 		
 	}
